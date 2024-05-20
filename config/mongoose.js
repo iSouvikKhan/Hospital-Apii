3d60e738a29ea43053ec2be2258ae307906c3293
isouvikkhan <souvik97381@gmail.com>
2024-05-20 09:58:16 +0530

    initial commit

---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const connection = "mongodb+srv://souvikmukherjee089:Souvik12345@hospital-api-cluster.5ppzfch.mongodb.net/?retryWrites=true&w=majority";
+
+// mongoose.connect('mongodb://127.0.0.1/HospitalAPI',{useNewUrlParser: true});
+
+mongoose.connect(connection, { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+db.once('open', function () {
+    console.log('Connected to Database :: MongoDB');
+});
+
+
+module.exports = db;
